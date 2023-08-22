1280152ec3c6faa533468a520e398321b6b0c538
ReBlast <reblastoff@gmail.com>
2023-08-22 22:49:47 +0500

    Initial commit.

---
+++ build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    kotlin("jvm") version "1.9.0"
+    application
+}
+
+group = "me.blast"
+version = "0.1"
+
+repositories {
+    mavenCentral()
+    maven("https://oss.sonatype.org/content/repositories/snapshots/")
+}
+
+dependencies {
+    implementation("org.javacord:javacord:3.9.0-SNAPSHOT")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    
+    implementation("org.apache.logging.log4j:log4j-to-slf4j:2.20.0")
+    implementation("ch.qos.logback:logback-classic:1.4.11")
+}
+
+kotlin {
+    jvmToolchain(8)
+}
