f429472ed09d587ae9d9029c942325d49efaeef2
ReBlast <reblastoff@gmail.com>
2023-08-26 00:55:18 +0500

    Remove unused exception. Document code.

--- src/main/kotlin/me/blast/parser/exceptions/ArgumentException.kt
+++ src/main/kotlin/me/blast/parser/exceptions/ArgumentException.kt
@@ -2,9 +2,26 @@ package me.blast.parser.exceptions
 
 import me.blast.command.CommandImpl.Delegate
 
-sealed class ArgumentException: Exception() {
-  data class Invalid(val argument: Delegate<*>): ArgumentException()
-  data class Undefined(val argument: Delegate<*>): ArgumentException()
-  data class Empty(val argument: Delegate<*>): ArgumentException()
-  data class Missing(val arguments: List<Delegate<*>>): ArgumentException()
+sealed class ArgumentException : Exception() {
+  
+  /**
+   * Represents an exception where an invalid value is provided for an argument.
+   *
+   * @property argument The argument with an invalid value.
+   */
+  data class Invalid(val argument: Delegate<*>) : ArgumentException()
+  
+  /**
+   * Represents an exception where an argument's value is empty.
+   *
+   * @property argument The argument with an empty value.
+   */
+  data class Empty(val argument: Delegate<*>) : ArgumentException()
+  
+  /**
+   * Represents an exception where one or more required arguments are missing.
+   *
+   * @property arguments The list of missing arguments.
+   */
+  data class Missing(val arguments: List<Delegate<*>>) : ArgumentException()
 }
