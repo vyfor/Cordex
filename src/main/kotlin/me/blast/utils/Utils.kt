71a83fe9180fae4feea45ecb7c6b45722007d823
ReBlast <reblastoff@gmail.com>
2023-08-26 20:52:34 +0500

    Add methods for generating usage and help messages.

--- src/main/kotlin/me/blast/utils/Utils.kt
+++ src/main/kotlin/me/blast/utils/Utils.kt
@@ -1,5 +1,11 @@
 package me.blast.utils
 
+import me.blast.command.Command
+import me.blast.command.CommandImpl
+import me.blast.parser.exceptions.ArgumentException
+import org.javacord.api.entity.message.MessageAuthor
+import org.javacord.api.entity.message.embed.EmbedBuilder
+import java.awt.Color
 import java.io.File
 import java.util.*
 
@@ -12,7 +18,6 @@ object Utils {
       if (!dir.exists()) {
         return classes
       }
-      
       val files = dir.listFiles()
       for (file in files!!) {
         if (file.isDirectory) {
@@ -34,7 +39,6 @@ object Utils {
       val resource = resources.nextElement()
       dirs.add(File(resource.file))
     }
-    
     val classes = mutableListOf<Class<*>>()
     for (dir in dirs) {
       classes.addAll(findClasses(dir, packageName))
@@ -82,5 +86,54 @@ object Utils {
   
   fun <T> Optional<T>.toNullable(): T? {
     return orElse(null)
+  }
+  
+  fun Command.generateHelpMessage(exception: ArgumentException, user: MessageAuthor) = EmbedBuilder().apply {
+    setTitle("Help for ${name.replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString() }} command")
+    if (aliases?.isNotEmpty() == true) addField("Aliases", aliases.joinToString(prefix = "`", separator = "`, `", postfix = "`"))
+    addField("Description", description)
+    setFooter(user.name, user.avatar)
+    setTimestampToNow()
+    setColor(Color.RED)
+    generateArgumentUsage(exception)?.let { addField("Arguments", it) }
+  }
+  
+  fun Command.generateArgumentUsage(exception: ArgumentException): String? {
+    return options.takeIf { it.isNotEmpty() }?.run {
+      val formattedArgs: String
+      val formattedOptions: String
+      when (exception) {
+        is ArgumentException.Invalid,
+        is ArgumentException.Empty,
+        -> {
+          partition { it is CommandImpl.PositionalDelegate || it is CommandImpl.PositionalDelegate<*>.OptionalPositionalDelegate<*, *> || it is CommandImpl.PositionalDelegate<*>.MultiplePositionalDelegate<*, *> }.apply {
+            formattedArgs = first.joinToString("\n") { option ->
+              "${if ((exception as ArgumentException.Invalid).argument == option) "\u001B[0;31m>>> " else ""}\u001B[0;36m[${if (option.isOptional || option is CommandImpl.FlagDelegate) "?" else "*"}]  \u001B[0;32m<${option.name}>\n     \u001B[0;30m${"${option.optionDescription}${if (option.defaultValue != null) " \u001B[0;30m(default: ${option.defaultValue})" else ""}"}"
+            }
+            formattedOptions = second.joinToString("\n") { option ->
+              "${if ((exception as ArgumentException.Invalid).argument == option) "\u001B[0;31m>>> " else ""}\u001B[0;36m[${if (option.isOptional || option is CommandImpl.FlagDelegate) "?" else "*"}]  \u001B[0;32m--${option.name}${if (option.short != null) ", -${option.short}" else ""}:\n     \u001B[0;30m${"${option.optionDescription}${if (option.defaultValue != null) " \u001B[0;30m(default: ${option.defaultValue})" else ""}"}"
+            }
+          }
+        }
+        
+        is ArgumentException.Missing -> {
+          partition { it is CommandImpl.PositionalDelegate || it is CommandImpl.PositionalDelegate<*>.OptionalPositionalDelegate<*, *> || it is CommandImpl.PositionalDelegate<*>.MultiplePositionalDelegate<*, *> }.apply {
+            formattedArgs = first.joinToString("\n") { option ->
+              "\u001B[0;36m[${if (option.isOptional || option is CommandImpl.FlagDelegate) "?" else "*"}]  \u001B[0;32m<${option.name}>\n     \u001B[0;30m${"${option.optionDescription}${if (option.defaultValue != null) "\u001B[0;30m (default: ${option.defaultValue})" else ""}"}"
+            }
+            formattedOptions = second.joinToString("\n") { option ->
+              "\u001B[0;36m[${if (option.isOptional || option is CommandImpl.FlagDelegate) "?" else "*"}]  \u001B[0;32m--${option.name}${if (option.short != null) ", -${option.short}" else ""}:\n     \u001B[0;30m${"${option.optionDescription}${if (option.defaultValue != null) " \u001B[0;30m(default: ${option.defaultValue})" else ""}"}"
+            }
+          }
+        }
+      }
+      "```ansi\n" + if (formattedArgs.isEmpty()) {
+        formattedOptions
+      } else if (formattedOptions.isEmpty()) {
+        formattedArgs
+      } else {
+        "\u001B[0;10mPositional Arguments:\n$formattedArgs\n\n\u001B[0;10mOptions:\n$formattedOptions"
+      } + "```"
+    }
   }
 }
