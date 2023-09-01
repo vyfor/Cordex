2441a4614efadd30cbf967818504f55a588ec62c
ReBlast <reblastoff@gmail.com>
2023-09-01 20:14:32 +0500

    Remove unnecessary comment.

--- src/main/kotlin/me/blast/command/Arguments.kt
+++ src/main/kotlin/me/blast/command/Arguments.kt
@@ -6,13 +6,6 @@ import me.blast.command.argument.Argument
 import me.blast.utils.Utils.lazyEmptyList
 
 class Arguments(@PublishedApi internal val args: Map<String, Any>) {
-//  inline operator fun <reified T> get(key: Argument<T>): T {
-//    return when(T::class) {
-//      Boolean::class -> args[key.argumentName] as? T ?: false as T
-//      List::class -> args[key.argumentName] as? T ?: lazyEmptyList as T
-//      else -> args[key.argumentName] as T
-//    }
-//  }
   operator fun <T> get(key: Argument<List<T>>): List<T> {
     return args[key.argumentName] as? List<T> ?: lazyEmptyList as List<T>
   }
