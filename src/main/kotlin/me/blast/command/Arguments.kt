af43cdbcfc56de5cc27ae58d72550813ac4378d3
ReBlast <reblastoff@gmail.com>
2023-09-12 23:48:47 +0500

    Remove unnecessary code.

--- src/main/kotlin/me/blast/command/Arguments.kt
+++ src/main/kotlin/me/blast/command/Arguments.kt
@@ -3,31 +3,18 @@
 package me.blast.command
 
 import me.blast.command.argument.Argument
-import me.blast.utils.Utils.lazyEmptyList
 
-class Arguments(@PublishedApi internal val args: Map<String, Any>) {
+class Arguments(@PublishedApi internal val args: Map<String, Any?>) {
   val <T> Argument<T>.value: T
     get() {
       return args[argumentName] as T
-    }
-  val <T> Argument<List<T>>.value: List<T>
-    get() {
-      return args[argumentName] as? List<T> ?: lazyEmptyList as List<T>
     }
   
   operator fun <T> Argument<T>.invoke(): T {
     return args[argumentName] as T
   }
   
-  operator fun <T> Argument<List<T>>.invoke(): List<T> {
-    return args[argumentName] as? List<T> ?: lazyEmptyList as List<T>
-  }
-  
   inline operator fun <T, R> Argument<T>.invoke(action: (T) -> R): R {
     return action(args[argumentName] as T)
-  }
-  
-  inline operator fun <T, R> Argument<List<T>>.invoke(action: (List<T>) -> R): R {
-    return action(args[argumentName] as? List<T> ?: lazyEmptyList as List<T>)
   }
 }
