f1eb6336434d82d3da7de74ee7225c698c4ccb7d
ReBlast <reblastoff@gmail.com>
2023-09-17 14:30:03 +0500

    Upgrade version to 0.3.1.

--- src/main/kotlin/me/blast/core/Cordex.kt
+++ src/main/kotlin/me/blast/core/Cordex.kt
@@ -19,7 +19,7 @@ import org.javacord.api.event.message.MessageCreateEvent
 import org.slf4j.LoggerFactory
 
 object Cordex {
-  const val VERSION = "0.3"
+  const val VERSION = "0.3.1"
   val logger = LoggerFactory.getLogger(Cordex::class.java)
   val scope = CoroutineScope(Dispatchers.Default)
 }
