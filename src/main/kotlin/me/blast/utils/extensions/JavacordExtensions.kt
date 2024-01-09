457356392b45abe151373f654f8c293649832766
ReBlast <reblastoff@gmail.com>
2024-01-09 12:37:09 +0500

    Small addition to JavacordExtensions.kt

--- src/main/kotlin/me/blast/utils/extensions/JavacordExtensions.kt
+++ src/main/kotlin/me/blast/utils/extensions/JavacordExtensions.kt
@@ -1,14 +1,20 @@
+@file:Suppress("unused")
+
 package me.blast.utils.extensions
 
 import org.javacord.api.entity.message.MessageFlag
 import org.javacord.api.entity.message.embed.EmbedBuilder
-import org.javacord.api.interaction.Interaction
 import org.javacord.api.interaction.InteractionBase
+import org.javacord.api.interaction.callback.InteractionImmediateResponseBuilder
 
 fun InteractionBase.respondEphemerally(content: String) {
   createImmediateResponder().setFlags(MessageFlag.EPHEMERAL).setContent(content).respond()
 }
 
-fun InteractionBase.respondEphemerally(embed: EmbedBuilder) {
-  createImmediateResponder().setFlags(MessageFlag.EPHEMERAL).addEmbed(embed).respond()
+fun InteractionBase.respondEphemerally(vararg embeds: EmbedBuilder) {
+  createImmediateResponder().setFlags(MessageFlag.EPHEMERAL).addEmbeds(*embeds).respond()
+}
+
+fun InteractionBase.respondEphemerally(block: InteractionImmediateResponseBuilder.() -> Unit) {
+  createImmediateResponder().apply(block).setFlags(MessageFlag.EPHEMERAL).respond()
 }
